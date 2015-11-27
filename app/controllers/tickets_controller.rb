13cab2a628aa1c80ad6bbd366227247d8a96ba57
Kenneth Lee <kennyxlee@gmail.com>
2015-11-26 19:16:21 -0800

    add: tickets#show + before_action :set_ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
   before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def new
     @ticket = @project.tickets.build
@@ -17,10 +18,17 @@ class TicketsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
     def set_project
       @project = Project.find(params[:project_id])
+    end
+
+    def set_ticket
+      @ticket = @project.tickets.find(params[:id])
     end
 
     def ticket_params
