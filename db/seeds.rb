1c88204149332d6946028b740a4a2744f271d4c8
Kenneth Lee <kennyxlee@gmail.com>
2015-11-28 19:50:48 -0800

    seed first admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless User.exists?(email: "admin@projecttracker.com")
+  User.create!(email: "admin@projecttracker.com", password: "password",
+               admin: true)
+end
+
+unless User.exists?(email: "viewer@projecttracker.com")
+  User.create!(email: "viewer@projecttracker.com", password: "password")
+end
+
+["Sublime Text 3", "Internet Explorer"].each do |name|
+  unless Project.exists?(name: name)
+    Project.create!(name: name, description: "A sample project about #{name}")
+  end
+end
