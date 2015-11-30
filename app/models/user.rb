2e4a41c2f5eff2baa56d034c70e6321dbf28ceb1
Kenneth Lee <kennyxlee@gmail.com>
2015-11-30 14:03:34 -0800

    add: admin may archive users (except her/himself)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # users that don't have an archvied_at date must be not-archived
+  scope :excluding_archived, lambda { where(archived_at: nil) }
 
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
+  end
+
+  def archive
+    self.update(archived_at: Time.now)
   end
 end
