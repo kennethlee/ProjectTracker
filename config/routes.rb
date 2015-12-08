f0f2904298cbeb9e0508fadfd440b0197fafa017
Kenneth Lee <kennyxlee@gmail.com>
2015-12-08 12:43:52 -0800

    add feature: authorized users can now leave comments on tickets

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,10 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
+  resources :tickets, only: [] do
+    resources :comments, only: [:create]
+  end
+
   resources :attachments, only: [:show, :new]
 
   # Example resource route with options:
