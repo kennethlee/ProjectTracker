1f0942dd6165ce6bef1fdc83559390197beb48af
Kenneth Lee <kennyxlee@gmail.com>
2015-12-09 01:04:22 -0800

    protect state_id from users who do not have authorization to change it

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -53,4 +53,6 @@ RSpec.configure do |config|
   # include Warden's helpers
   config.include Warden::Test::Helpers, type: :feature
   config.after(type: :feature) { Warden.test_reset! }
+
+  config.include Devise::TestHelpers, type: :controller
 end
